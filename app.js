460c6b1c2b74bdeba87e4f386edd7bcf4c041cc4
Aashiq <md.ashikuzzaman.1429@gmail.com>
2019-01-31 01:20:04 +0600

    Added edit and delete button accordingly, need to add delete and edit functionality

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var User = require('./models/user'),
     Post = require('./models/post'),
     Comment = require('./models/comment');
 
+var middleware = require("./middleware");
+
 
 //=======
 //CLEAN UP
@@ -18,6 +20,14 @@ var User = require('./models/user'),
 //   if (err) console.log(err);
 //   else{
 //     console.log('cleaned Post data');
+    
+//   }
+// });
+
+// User.deleteMany({},(err,data)=>{
+//   if (err) console.log(err);
+//   else{
+//     console.log('cleaned User');
     
 //   }
 // });
@@ -69,31 +79,65 @@ app.use((req, res, next) => {
 //Routes
 // ================
 app.get("/", function (req, res) {
-  Post.find({}, (err, blogData)=>{
-    if(err) console.log(err);
-    else{
-      res.render("posts/index",{ v_blogData: blogData });
+  res.redirect("/posts");
+
+});
+
+app.get("/posts",function (req, res) {
+  Post.find({}, (err, blogData) => {
+    if (err) console.log(err);
+    else {
+      res.render("posts/index", { v_blogData: blogData });
     }
-    
   });
 });
 
-app.get("/posts", function (req, res) {
-  res.redirect("/");
-});
+app.post("/posts",middleware.isLoggedIn, function (req, res) {
+  var title = req.body.blog.title;
+  var description = req.body.blog.description;
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
 
-app.post("/posts", function (req, res) {
-  // console.log(req.body.blog);
-  Post.create(req.body.blog, (err, blogData)=>{
+  var newPost = { title: title, description: description, author: author };
+  // console.log(newPost);
+  
+  // res.send("posting");
+  // console.log(req.body);
+
+  Post.create(newPost, (err, blogData)=>{
     if(err) console.log(err);
     else{
+      console.log(newPost);
       res.redirect('/');
     }    
   });
 });
 
-app.get("/posts/new", function (req, res) {
+app.get("/posts/new", middleware.isLoggedIn, function (req, res) {
   res.render("posts/new");
+});
+
+
+app.get("/posts/:username/:id_user/", middleware.isLoggedIn, function (req, res) {
+  // res.send(req.params.id_user);
+  if(req.user._id.equals(req.params.id_user)){
+    Post.find({ 'author.id': req.params.id_user }, (err, posts) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      }
+      else {
+        // console.log(posts);
+        res.render("posts/owned", { v_posts: posts });
+
+      }
+    });
+  }
+  else{
+    res.send('You are not authorized');
+  }
 });
 
 app.get("/posts/:id_post", function (req, res) {
@@ -105,6 +149,7 @@ app.get("/posts/:id_post", function (req, res) {
     }
   });
 });
+
 
 app.get("/posts/:id_post/comments/new", function (req, res) {
   res.render("comments/new",{id: req.params.id_post});
@@ -138,10 +183,36 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.post("/register", function (req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      req.flash("error", err.message);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function () {
+      // req.flash("success", "Successfully signed up" + user.username);
+      res.redirect("/posts");
+    });
+  });
+});
+
 app.get("/login", function (req, res) {
   res.render("login");
 });
 
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/posts",
+    failureRedirect: "/login"
+  }), function (req, res) {
+  });
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  // req.flash("success", "Logged you out!");
+  res.redirect("/posts");
+});
 
 app.listen(3000, () => {
   console.log("Server is running");
